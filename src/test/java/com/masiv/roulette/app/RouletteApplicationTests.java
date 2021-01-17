78e019f153fccd0f104e24eebdbd7acb65f3cced
Jeisson Barrero <jgb_17@hotmail.com>
2021-01-16 22:01:14 -0500

    first commit

---
+++ src/test/java/com/masiv/roulette/app/RouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masiv.roulette.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
